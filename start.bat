e4f78498972ea826be20bc2879b64280db7290a6
blueroler <107337300+blueroler@users.noreply.github.com>
2023-01-09 22:21:49 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -14,7 +14,8 @@ ICACLS C:\Windows\installer /grant admin:F >nul
 echo Successfully Installed!, If the RDP is Dead, Please Rebuild Again!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url
-set hosting=tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url
+set hosting=local
+tasklist | find /i "ngrok.exe" && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url
 echo %hosting%
 start https://script.google.com/macros/s/AKfycbwOqJwESaIOZdJ3yKOkK7W8x9XUkN_PhxTi4YHnz8CHAyyc484/exec?mess=lol
 echo Username: admin
