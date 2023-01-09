519dbb9e90a3bb9a2a8384751f2e6b56d5c1752b
blueroler <107337300+blueroler@users.noreply.github.com>
2023-01-10 00:44:17 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -12,7 +12,7 @@ sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant admin:F >nul
 ICACLS C:\Windows\installer /grant admin:F >nul
 tasklist | find /i "ngrok.exe" >nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url
-tasklist | find /i "ngrok.exe" >nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url >set hosting
+tasklist | find /i "ngrok.exe" >nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url >hosting
 echo %hosting%
 start https://script.google.com/macros/s/AKfycbwOqJwESaIOZdJ3yKOkK7W8x9XUkN_PhxTi4YHnz8CHAyyc484/exec?mess=lol
 echo Username: admin
