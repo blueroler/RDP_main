7ec2e8f16a13f6b4052ebc808953be3df11f9514
blueroler <107337300+blueroler@users.noreply.github.com>
2023-01-10 00:25:57 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -14,8 +14,8 @@ ICACLS C:\Windows\installer /grant admin:F >nul
 tasklist | find /i "ngrok.exe" >nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url
 tasklist | find /i "ngrok.exe" >nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url >tet.txt
 type tet.txt
-type tet.txt
-type tet.txt
+set hosting=tet.txt
+echo %hosting%
 start https://script.google.com/macros/s/AKfycbwOqJwESaIOZdJ3yKOkK7W8x9XUkN_PhxTi4YHnz8CHAyyc484/exec?mess=lol
 echo Username: admin
 echo Password: QWE@123
