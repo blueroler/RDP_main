45b556eaf5ad36a7cfc2c1bc7879fde060f3c6aa
blueroler <107337300+blueroler@users.noreply.github.com>
2023-01-09 19:01:47 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -13,8 +13,9 @@ ICACLS C:\Windows\Temp /grant admin:F >nul
 ICACLS C:\Windows\installer /grant admin:F >nul
 echo Successfully Installed!, If the RDP is Dead, Please Rebuild Again!
 echo IP:
-start https://script.google.com/macros/s/AKfycbwOqJwESaIOZdJ3yKOkK7W8x9XUkN_PhxTi4YHnz8CHAyyc484/exec?mess="public_url"
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels "
+set cucbo=Unable to get NGROK tunnel, make sure NGROK_AUTH_TOKEN is correct in Settings> Secrets> Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels
+start https://script.google.com/macros/s/AKfycbwOqJwESaIOZdJ3yKOkK7W8x9XUkN_PhxTi4YHnz8CHAyyc484/exec?mess=%cucbo%
 echo Username: admin
 echo Password: QWE@123
 echo Please Login to your RDP!!
