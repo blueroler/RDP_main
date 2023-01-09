d3f486a863eec4e4ad53456b19d2224709a4c204
blueroler <107337300+blueroler@users.noreply.github.com>
2023-01-09 23:33:09 +0700

    Update start.bat

--- start.bat
+++ start.bat
@@ -11,13 +11,11 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant admin:F >nul
 ICACLS C:\Windows\installer /grant admin:F >nul
-echo Successfully Installed!, If the RDP is Dead, Please Rebuild Again!
-echo IP:
+:check
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url
-tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels
+set hosting=:check
 echo %hosting%
 start https://script.google.com/macros/s/AKfycbwOqJwESaIOZdJ3yKOkK7W8x9XUkN_PhxTi4YHnz8CHAyyc484/exec?mess=lol
 echo Username: admin
 echo Password: QWE@123
-echo Please Login to your RDP!!
 ping -n 10 127.0.0.1 >nul
