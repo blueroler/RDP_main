c0805b8ff3b5cfb6dd93a545a98edb98c72a4644
blueroler <107337300+blueroler@users.noreply.github.com>
2023-01-11 22:50:57 +0700

    Update loop.bat

--- loop.bat
+++ loop.bat
@@ -3,6 +3,6 @@ echo Blueroler DEV
 goto check
 tasklist | find /i "ngrok.exe" >Nul && goto check || echo "Unable to get NGROK tunnel, be sure NGROK_AUTH_TOKEN right in Settings> Secrets> Repository secret. Maybe your previous VM is still running: https://dashboard.ngrok.com/status/tunnels" & ping 127.0.0.1 >Nul & exit
 :check
-ping 127.0.0.1 -t
+ping 8.8.8.8 -t
 cls
 goto check
